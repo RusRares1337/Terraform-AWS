7e5657b4c3a9aa6f446a5610abd5aca2b60279fa
RusRares1337 <rusrares1997@gmail.com>
2023-05-14 16:49:27 +0300

    changes

--- main.tf
+++ main.tf
@@ -4,28 +4,78 @@ provider "aws" {
     secret_key = "X7QgO50UMC9MYcdkJCBqIdtQsmvK+LyTlNrj8M3P"
 }
 
-variable "cidr_blocks" {
-    description = "cidr block and name tags for vpc and subnets"
-    type = list(object({
-        cidr_block= string
-        name = string
-    }))
-}
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
 
 
-resource "aws_vpc" "development-vpc" {
-    cidr_block = var.cidr_blocks[0].cidr_block
+resource "aws_vpc" "myapp-vpc" {
+    cidr_block = var.vpc_cidr_block
     tags = {
-        Name = var.cidr_blocks[0].name 
+        Name = "${var.env_prefix}-vpc"
     }
 }
 
-resource "aws_subnet" "dev-subnet-1" {
-    vpc_id = aws_vpc.development-vpc.id
-    cidr_block = var.cidr_blocks[1].cidr_block
-    availability_zone = "eu-west-3a"
+resource "aws_subnet" "myapp-subnet-1" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    cidr_block = var.subnet_cidr_block
+    availability_zone = var.avail_zone
     tags = {
-        Name = var.cidr_blocks[1].name
+        Name = "${var.env_prefix}-subnet-1"
     }
 }
 
+resource "aws_route_table" "myapp-route-table" {
+    vpc_id = aws_vpc.myapp-vpc.id
+
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-internet-gateway.id
+    }
+    tags = {
+        Name = "${var.env_prefix}-route-table"
+    }
+}
+
+resource "aws_internet_gateway" "myapp-internet-gateway" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    tags = {
+        Name = "${var.env_prefix}-internet-gateway"
+    }
+}
+
+resource "aws_route_table_association" "a-rtb-subnet" {
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    route_table_id = aws_route_table.myapp-route-table.id
+}
+
+resource "aws_security_group" "myapp-sg" {
+    name = "myapp-sg"
+    vpc_id = aws_vpc.myapp-vpc.id
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["82.78.49.157/32"]
+    }
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        prefix_list_ids = []
+    }
+    tags = {
+        Name = "${var.env_prefix}-sg"
+    }
+}
