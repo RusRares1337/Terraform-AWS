58caefe229dd1c40352ee68683919a9afdc6969d
RusRares1337 <rusrares1997@gmail.com>
2023-05-14 18:31:06 +0300

    automated the part of creating server key

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
     region = "eu-west-3"
-    access_key = "AKIASGALKZKSFYLVNGKC"
-    secret_key = "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"
 }
 
 variable vpc_cidr_block {}
@@ -10,6 +8,7 @@ variable avail_zone {}
 variable env_prefix {}
 variable my_ip {}
 variable instance_type {}
+variable public_key_location {}
 
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
@@ -93,6 +92,18 @@ data "aws_ami" "latest-amazon-linux-image" {
     }
 }
 
+output "aws_ami_id" {
+    value = data.aws_ami.latest-amazon-linux-image.id
+}
+
+output "ec2_public_ip" {
+    value = aws_instance.myapp-server
+}
+resource "aws_key_pair" "ssh-key" {
+    key_name = "server-key"
+    public_key = file(var.public_key_location)
+}
+
 resource "aws_instance" "myapp-server" {
     ami = data.aws_ami.latest-amazon-linux-image.id
     instance_type = var.instance_type
@@ -102,7 +113,7 @@ resource "aws_instance" "myapp-server" {
     availability_zone = var.avail_zone
 
     associate_public_ip_address = true
-    key_name = "server-key-pair"
+    key_name = aws_key_pair.ssh-key.key_name
 
     tags = {
         Name = "${var.env_prefix}-server"
