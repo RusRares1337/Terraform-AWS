81f8ea6d5c39a129d71c3e137db55fd3ac86276f
Rus Rares <rusrares1997@gmail.com>
2024-02-07 15:17:17 +0200

    added webserver module

---
+++ modules/webserver/variables.tf
@@ -0,0 +1,9 @@
+variable image_name {}
+variable vpc_id {}
+variable my_ip {}
+variable env_prefix {}
+variable public_key_location {}
+variable instance_type {}
+variable subnet_id {}
+variable avail_zone {}
+
